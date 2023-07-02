565a528debc2761aca5d0a0b8aea5ddc9a00ddf4
tienvu96 <vuanhtien02081996@gmail.com>
2023-07-02 16:50:04 +0700

    configured docker compose and check to wait for database.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    """add number x and y and return."""
+    return x + y
+
+
+def subtract(x, y):
+    """abstract number x and y and return"""
+    if x > y:
+        return x - y
+    else:
+        return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalTests(SimpleTestCase):
+    """test the calc module."""
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """test subtract number together."""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
